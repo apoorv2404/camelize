0a81c77b57aeee054a220424f1c891f03c3025ea
Apoorv Agrawal <apoorv.agrawal@outlook.com>
2016-06-01 13:50:59 +0530

    AMD support

--- index.js
+++ index.js
@@ -1,75 +1,88 @@
-module.exports = function(obj, options) {
-  options = options || {};
-  if (typeof obj === 'string') return camelCase(obj, options);
-  return walk(obj, options);
-};
-
-function walk(obj, options) {
-  if (!obj || typeof obj !== 'object') return obj;
-  if (isDate(obj) || isRegex(obj)) return obj;
-  if (isArray(obj)) return map(obj, function(obj) {
-    return walk(obj, options);
-  });
-  return reduce(objectKeys(obj), function(acc, key) {
-    var camel = camelCase(key, options);
-    acc[camel] = walk(obj[key], options);
-    return acc;
-  }, {});
-}
-
-function camelCase(str, options) {
-
-  if (options.ignore && options.ignore.indexOf(str) !== -1) {
-    return str;
+(function(root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    define([], factory);
+  } else if (typeof module === 'object' && module.exports) {
+    module.exports = factory();
+  } else {
+    root.camelize = factory();
   }
 
-  if (options.ignoreRegex) {
-    for (var i = 0; i < options.ignoreRegex.length; ++i) {
-      if (options.ignoreRegex[i].test(str)) {
-        return str;
+}(this, function() {
+
+  return function(obj, options) {
+    options = options || {};
+    if (typeof obj === 'string') return camelCase(obj, options);
+    return walk(obj, options);
+  };
+
+  function walk(obj, options) {
+    if (!obj || typeof obj !== 'object') return obj;
+    if (isDate(obj) || isRegex(obj)) return obj;
+    if (isArray(obj)) return map(obj, function(obj) {
+      return walk(obj, options);
+    });
+    return reduce(objectKeys(obj), function(acc, key) {
+      var camel = camelCase(key, options);
+      acc[camel] = walk(obj[key], options);
+      return acc;
+    }, {});
+  }
+
+  function camelCase(str, options) {
+
+    if (options.ignore && options.ignore.indexOf(str) !== -1) {
+      return str;
+    }
+
+    if (options.ignoreRegex) {
+      for (var i = 0; i < options.ignoreRegex.length; ++i) {
+        if (options.ignoreRegex[i].test(str)) {
+          return str;
+        }
       }
     }
+
+    return str.replace(/[_.-](\w|$)/g, function(_, x) {
+      return x.toUpperCase();
+    });
   }
 
-  return str.replace(/[_.-](\w|$)/g, function(_, x) {
-    return x.toUpperCase();
-  });
-}
+  var isArray = Array.isArray || function(obj) {
+    return Object.prototype.toString.call(obj) === '[object Array]';
+  };
 
-var isArray = Array.isArray || function(obj) {
-  return Object.prototype.toString.call(obj) === '[object Array]';
-};
+  var isDate = function(obj) {
+    return Object.prototype.toString.call(obj) === '[object Date]';
+  };
 
-var isDate = function(obj) {
-  return Object.prototype.toString.call(obj) === '[object Date]';
-};
+  var isRegex = function(obj) {
+    return Object.prototype.toString.call(obj) === '[object RegExp]';
+  };
 
-var isRegex = function(obj) {
-  return Object.prototype.toString.call(obj) === '[object RegExp]';
-};
+  var has = Object.prototype.hasOwnProperty;
+  var objectKeys = Object.keys || function(obj) {
+    var keys = [];
+    for (var key in obj) {
+      if (has.call(obj, key)) keys.push(key);
+    }
+    return keys;
+  };
 
-var has = Object.prototype.hasOwnProperty;
-var objectKeys = Object.keys || function(obj) {
-  var keys = [];
-  for (var key in obj) {
-    if (has.call(obj, key)) keys.push(key);
+  function map(xs, f) {
+    if (xs.map) return xs.map(f);
+    var res = [];
+    for (var i = 0; i < xs.length; i++) {
+      res.push(f(xs[i], i));
+    }
+    return res;
   }
-  return keys;
-};
 
-function map(xs, f) {
-  if (xs.map) return xs.map(f);
-  var res = [];
-  for (var i = 0; i < xs.length; i++) {
-    res.push(f(xs[i], i));
+  function reduce(xs, f, acc) {
+    if (xs.reduce) return xs.reduce(f, acc);
+    for (var i = 0; i < xs.length; i++) {
+      acc = f(acc, xs[i], i);
+    }
+    return acc;
   }
-  return res;
-}
 
-function reduce(xs, f, acc) {
-  if (xs.reduce) return xs.reduce(f, acc);
-  for (var i = 0; i < xs.length; i++) {
-    acc = f(acc, xs[i], i);
-  }
-  return acc;
-}
+}));
